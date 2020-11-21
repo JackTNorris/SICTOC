129cdc4608013cf21bb0f32f2f8c03e67627af45
Jack Norris <jacknorris@Jacks-MacBook-Pro.local>
2020-11-20 19:31:17 -0600

    First commit

---
+++ android/app/src/main/java/com/cerca_scholar_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cerca_scholar_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Cerca_Scholar_App";
+  }
+}
